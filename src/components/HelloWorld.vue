71ced7f04b959f8a90416709ad6143ee2ccd53ea
RoxySmash <julia.ppsc@gmail.com>
2018-07-20 21:38:11 +0200

    Add Axios HTTP client to consume and display data from API request

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,104 +1,47 @@
 <template>
-  <div class="hello container justify-content-center text-center">
-    <img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <span class="badge badge-pill badge-primary">Primary</span>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+<div> Derp, works </div>
+  <!-- <div class="container justify-content-center">
+  <div class="card" v-for="item in info" :key="item.id" style="width: 18rem;">
+  <img class="card-img-top" v-bind:src="item.images[0]" alt="Cat">
+  <div class="card-body">
+    <h5 class="card-title">{{ item.name }}</h5>
+    <p class="card-text">{{ item.bio }}</p>
   </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Dislike</a>
+    <a href="#" class="card-link">Superlike</a>
+     <a href="#" class="card-link">Like</a>
+  </div>
+</div>
+</div> -->
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+// import axios from 'axios'
+
+// export default {
+//   name: 'app',
+//   data () {
+//     return {
+//       info: null
+//     }
+//   },
+//   mounted () {
+//     axios
+//       .get('http://my-json-server.typicode.com/airtame/kittens/kittens/')
+//       .then(response => (this.info = response.data))
+//   }
+// }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+/* h1, h2 {
   font-weight: normal;
 }
 ul {
@@ -111,5 +54,5 @@ li {
 }
 a {
   color: #42b983;
-}
+} */
 </style>
