56106b1578fc1602c82fb19d7d05917b64a2ed25
RoxySmash <julia.ppsc@gmail.com>
2018-07-20 22:39:22 +0200

    Create 'data' property and add 'mounted' lifecycle hook to retrieve json

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,33 @@
 <template>
-<div> Derp, works </div>
-  <!-- <div class="container justify-content-center">
-  <div class="card" v-for="item in info" :key="item.id" style="width: 18rem;">
-  <img class="card-img-top" v-bind:src="item.images[0]" alt="Cat">
-  <div class="card-body">
-    <h5 class="card-title">{{ item.name }}</h5>
-    <p class="card-text">{{ item.bio }}</p>
+  <div id="app">
+    <div class="container text-center w-25 p-3">
+      <span v-for="item in info" :key="item.id">
+        <p><b>{{item.name}}</b></p>
+        <img style="width:18rem" v-bind:src="item.images[0]"> <br/>
+        <p><i>Hobbies: {{ item.bio }}</i></p>
+        <hr/>
+        </span>
+    </div>
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="card-link">Dislike</a>
-    <a href="#" class="card-link">Superlike</a>
-     <a href="#" class="card-link">Like</a>
-  </div>
-</div>
-</div> -->
 </template>
 
 <script>
-// import axios from 'axios'
-
-// export default {
-//   name: 'app',
-//   data () {
-//     return {
-//       info: null
-//     }
-//   },
-//   mounted () {
-//     axios
-//       .get('http://my-json-server.typicode.com/airtame/kittens/kittens/')
-//       .then(response => (this.info = response.data))
-//   }
-// }
-
+import axios from 'axios'
+export default {
+  name: 'app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://my-json-server.typicode.com/airtame/kittens/kittens/')
+      .then(response => (this.info = response.data))
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
 </style>
