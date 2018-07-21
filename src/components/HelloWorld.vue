316518d461b49901c49fe499a5fd4fbaddadaa0e
RoxySmash <julia.ppsc@gmail.com>
2018-07-21 13:52:14 +0200

    Display error message when data cannot be retrieved from API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container">
+    <!-- Condition for displaying error message-->
+    <section class="text-center mt-5" v-if="errored">
+      <h2>
+        <p>Oh noes! </p>
+      </h2> <br/> The kittens must be having a nap because we can't find any! <br/>
+      <p>Please try again in a bit or navigate here</p>
+    </section>
+    <!-- Kitten card section -->
     <section class="kitten-card mt-5">
       <div class="row">
         <div class="col-md-10 offset-sm-1">
@@ -30,23 +38,39 @@
         </div>
       </div>
     </section>
+    <!-- End of kitten card section -->
   </div>
 </template>
 
 <script>
 /*eslint-disable*/
   import axios from "axios";
+
   export default {
     name: "app",
+    // Create data property for our info
     data() {
       return {
-        info: null
+        info: null,
+        loading: true,
+        errored: false
       };
     },
+    // Retrieve data and assign it using the 'mounted' lifecycle hook
     mounted() {
       axios
-        .get("http://my-json-server.typicode.com/airtame/kittens/kittens/")
-        .then(response => (this.info = response.data));
+        //.get("http://my-json-server.typicode.com/airtame/kittens/kittens")
+        .get("http://my-json-server.typicode.com")
+        .then(response => {
+          this.info = response.data
+        })
+        // Display error message if information cannot be retrieved
+        .catch(error => {
+          console.log(error, 'Cannot retrieve data, initiate panic mode!')
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+  
     }
   };
 </script>
